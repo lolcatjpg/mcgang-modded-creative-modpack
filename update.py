e36abc1852aeb0adca3413189ab04c680e6a7dcb
lolcatproductions <66800621+lolcatproductions@users.noreply.github.com>
2024-04-23 00:51:41 +0200

    remove newline from update.py

--- update.py
+++ update.py
@@ -4,4 +4,3 @@ from tkinter import messagebox
 update = messagebox.askokcancel('update', 'Update modpack?')
 if update == True:
     subprocess.run('git fetch origin main && git reset --hard origin/main', shell=True)
-
